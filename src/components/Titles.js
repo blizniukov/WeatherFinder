e192aec7ed299a5a429c5078056d026c9323be61
Nikita Blizniukov <blizniukov@gmail.com>
2019-02-20 15:45:35 +0200

    Added styles

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import {Jumbotron} from 'reactstrap';
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out, temperature, conditions and more...</p>
-            </div>
-        );
-    }
-}
+const Titles = () => (
+    <Jumbotron style={{textAlign: 'center'}}>
+        <h1 className="display-3">Weather Finder</h1>
+        <p className="lead">Find out, temperature, conditions and more...</p>
+    </Jumbotron>
+);
 
 export default Titles;
